5d23e2ff6130b63b5552be2ad2f4e6da7be877c5
maharaj <thishukumarab77@gmail.com>
2025-06-15 16:08:44 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 setup(
-    name="GenerativeAIProject",
-    version="0.0.0",
-    author="Thishukumar maharaj",
-    author_email="thishukumarab77@gmail.com",
+    name='Generative AI Project',
+    version='0.0.0',
+    author='Thishukumar maharaj',
+    author_email='thishukumarab77@gmail.com',
     packages=find_packages(),
     install_requires=[]
 )
---
+++ src/prompt.py
@@ -0,0 +1,9 @@
+
+system_prompt=(
+    "You are a helpful medical assistant. "
+    "You will be provided with some medical documents. "
+    "Use the information in these documents to answer the user's questions."
+    "if you don't know the answer, just say 'I don't know'. use three sentences maximum to answer concise. "
+    "\n\n"
+    "{context}"
+)
